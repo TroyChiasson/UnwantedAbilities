62ecaa5f22d223ce4a14781c5587ffc17ed8152d
s0nicx2729 <157264174+s0nicx2729@users.noreply.github.com>
2024-04-13 15:09:14 -0500

    Basic Interactable functionality

---
+++ UnwantedAbilities/Assets/Scripts/InteractTest.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractTest : MonoBehaviour
+{
+    private Input @input;
+    public GameObject interact;
+    private bool inRange;
+    // Start is called before the first frame update
+    void Start()
+    {
+        input = new Input();
+        input.Enable();
+        inRange = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (input.Default.Interact.triggered && inRange){
+            Destroy(interact);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("PlayerEdward")){
+            inRange = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        if (other.CompareTag("PlayerEdward")){
+            inRange = false; 
+        }
+    }
+}
