783f1c6958d405b17fa3f3e6eb73e62204c58ea9
s0nicx2729 <157264174+s0nicx2729@users.noreply.github.com>
2024-04-13 15:45:27 -0500

    Interactable Replacement code

--- UnwantedAbilities/Assets/Scripts/InteractTest.cs
+++ UnwantedAbilities/Assets/Scripts/InteractTest.cs
@@ -6,6 +6,8 @@ public class InteractTest : MonoBehaviour
 {
     private Input @input;
     public GameObject interact;
+    public GameObject replacement;
+    private Vector2 pos;
     private bool inRange;
     // Start is called before the first frame update
     void Start()
@@ -13,6 +15,7 @@ public class InteractTest : MonoBehaviour
         input = new Input();
         input.Enable();
         inRange = false;
+        pos = transform.position;
     }
 
     // Update is called once per frame
@@ -20,6 +23,7 @@ public class InteractTest : MonoBehaviour
     {
         if (Game.Instance.input.Default.Interact.triggered && inRange){
             Destroy(interact);
+            replacement.transform.position = new Vector2(pos.x, pos.y);
         }
     }
 
