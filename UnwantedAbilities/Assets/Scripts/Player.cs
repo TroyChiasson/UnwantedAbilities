6ee247db637aad6ea61057f340d5c294e8bc44fc
s0nicx2729 <157264174+s0nicx2729@users.noreply.github.com>
2024-04-13 21:37:27 -0500

    Water test commits

--- UnwantedAbilities/Assets/Scripts/Player.cs
+++ UnwantedAbilities/Assets/Scripts/Player.cs
@@ -6,17 +6,18 @@ public class Player : MonoBehaviour
 {
 
     //x movement speed
-    private float movementSpeed = 5f;
+    public static float movementSpeed = 5f;
 
     //y movement speed
-    private float jumpSpeed = 10f;
+    public static float jumpSpeed = 10f;
 
     //longest amount of time the jump key is allowed to be held
-    private float jump_tot_time = 0.5f;
+    public static float jump_tot_time = 0.5f;
 
     //how long the jump key has been held for
-    private float jump_cur_time = 0f;
+    public static float jump_cur_time = 0f;
 
+    public static double health;
     //keyboard presses
     private bool upIsHeld = false;
     private bool downIsHeld = false;
@@ -26,26 +27,17 @@ public class Player : MonoBehaviour
 
     private int jumpsAvailable;
     private int maxJumps = 1;
-
-    public BoxCollider2D bc;
     public Rigidbody2D rb;
-    public BuoyancyEffector2D buoyancy;
-    public GameObject water;
-    public PolygonCollider2D waterCollider;
-	
-    public double Stamina;
+    public static double stamina;
     public LayerMask groundLayer;
     private float raycastDistance = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        Stamina = 100;
-        water = GameObject.FindWithTag("ShouldBouyant");
-        buoyancy = water.GetComponent<BuoyancyEffector2D>();
-        waterCollider = water.GetComponent<PolygonCollider2D>();
+        stamina = 100;
+        health = 100;
         rb = GetComponent<Rigidbody2D>();
-        bc = GetComponent<BoxCollider2D>();
         ResetJumps();
     }
 
@@ -64,12 +56,20 @@ public class Player : MonoBehaviour
         rb.velocity = curVel;
     }
 
-    // Update is called once per frame
-    void Update() {
+    /*public void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Water")) {
+            rb.gravityScale = 0;
+        }
+    }
 
-        if (!bc.IsTouching(waterCollider))
-        {
-            
+    public void OnTriggerExit2D(Collider2D other) {
+        if (other.CompareTag("Water")) {
+            rb.gravityScale = 1;
+        }
+    }*/
+
+    // Update is called once per frame
+    void Update() {          
             if (Physics2D.Raycast(transform.position, Vector2.down, raycastDistance, groundLayer)) {
                 jumpsAvailable = maxJumps;
             }
@@ -139,120 +139,6 @@ public class Player : MonoBehaviour
                     ResetYVelocity();
                     rb.AddForce(Vector2.down * 5f, ForceMode2D.Impulse);
                 }
-
             }
-        }
-
-        //SWIMMING MOVEMENT
-        if (bc.IsTouching(waterCollider))
-        {
-            //CHECK IF DROWNING
-            if (bc.transform.localPosition.y < waterCollider.transform.localPosition.y + 8)
-            {
-                Stamina += -.1;
-            }
-            else
-            {
-                Stamina = 100;
-            }
-
-            if (Stamina < 0)
-            {
-                print("death");
-            }
-
-
-            //rb.AddForce(new Vector2(0, -1f) * rb.mass * 1.2f);
-
-            // W key
-            if (Game.Instance.input.Default.MoveUp.WasPressedThisFrame()) { upIsHeld = true; }
-            if (Game.Instance.input.Default.MoveUp.WasReleasedThisFrame()) { upIsHeld = false; }
-
-            // move Up
-            if (upIsHeld)
-            {
-                var curVector = transform.localPosition;
-                curVector.y = curVector.y + movementSpeed * Time.deltaTime;
-                transform.localPosition = curVector;
-            }
-
-            // S key
-            if (Game.Instance.input.Default.MoveDown.WasPressedThisFrame()) { downIsHeld = true; }
-            if (Game.Instance.input.Default.MoveDown.WasReleasedThisFrame()) { downIsHeld = false; }
-
-            // move left
-            if (downIsHeld)
-            {
-                var curVector = transform.localPosition;
-                curVector.y = curVector.y - movementSpeed * Time.deltaTime;
-                transform.localPosition = curVector;
-            }
-
-            // A key
-            if (Game.Instance.input.Default.MoveLeft.WasPressedThisFrame()) { leftIsHeld = true; }
-            if (Game.Instance.input.Default.MoveLeft.WasReleasedThisFrame()) { leftIsHeld = false; }
-
-            // move left
-            if (leftIsHeld)
-            {
-                var curVector = transform.localPosition;
-                curVector.x = curVector.x - movementSpeed * Time.deltaTime;
-                transform.localPosition = curVector;
-            }
-
-            // D key
-            if (Game.Instance.input.Default.MoveRight.WasPressedThisFrame()) { rightIsHeld = true; }
-            if (Game.Instance.input.Default.MoveRight.WasReleasedThisFrame()) { rightIsHeld = false; }
-
-            // move right
-            if (rightIsHeld)
-            {
-                var curVector = transform.localPosition;
-                curVector.x = curVector.x + movementSpeed * Time.deltaTime;
-                transform.localPosition = curVector;
-            }
-
-            // space key
-            if (Game.Instance.input.Default.Jump.WasPressedThisFrame() && jumpsAvailable > 0)
-            {
-                jumpIsHeld = true;
-                jump_cur_time = 0f;
-                jumpsAvailable--;
-
-                ResetYVelocity();
-                rb.AddForce(Vector2.up * 10f, ForceMode2D.Impulse);
-            }
-            if (Game.Instance.input.Default.Jump.WasReleasedThisFrame() && jumpIsHeld)
-            {
-                jumpIsHeld = false;
-
-                ResetYVelocity();
-                rb.AddForce(Vector2.down * 5f, ForceMode2D.Impulse);
-            }
-
-            // move up
-            // ***NOTE rigid body is affected by gravity automatically in unity
-            //         gravity is exponential, this movement up is linear
-            //         if there is choppy jump movement this is why
-            if (jumpIsHeld)
-            {
-                //var curVector = transform.localPosition;
-                //curVector.y = curVector.y + jumpSpeed * Time.deltaTime;
-                //transform.localPosition = curVector;
-
-                //you cant hold jump forever
-                var vel = rb.velocity;
-                if (vel.y <= 0)
-                {
-                    jumpIsHeld = false;
-                    ResetYVelocity();
-                    rb.AddForce(Vector2.down * 5f, ForceMode2D.Impulse);
-                }
-
-            }
-        }
-
     }
-
-
 }
