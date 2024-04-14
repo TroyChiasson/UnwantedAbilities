e203134f08b447c17367587e0d1639bfc895164f
ebm04 <139780145+ebm04@users.noreply.github.com>
2024-04-14 13:26:27 -0500

    Mainmenu fixes and intro scene

--- UnwantedAbilities/Assets/Scripts/MainMenu.cs
+++ UnwantedAbilities/Assets/Scripts/MainMenu.cs
@@ -14,7 +14,7 @@ public class MainMenu : MonoBehaviour
 
     public void StartGame()
     {
-        SceneManager.LoadScene("Main Scene");
+        SceneManager.LoadScene("Intro");
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
