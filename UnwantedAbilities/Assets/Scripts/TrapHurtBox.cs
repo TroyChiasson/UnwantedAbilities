5af104c4edbb292c414393deb0595793a26f878e
Troy Chiasson <chiassonmtroy@gmail.com>
2024-04-14 07:39:01 -0500

    Fixed fire ticking damage

--- UnwantedAbilities/Assets/Scripts/TrapHurtBox.cs
+++ UnwantedAbilities/Assets/Scripts/TrapHurtBox.cs
@@ -7,13 +7,29 @@ public class TrapHurtBox : MonoBehaviour
 {
     // Object
     public Player player;
-
+    public static bool inTrap;
     public int trapDamage; 
 
+
+    public int frameTimer = 1;
+    void Update() {
+        frameTimer++;
+        if (inTrap == true && frameTimer % 10 == 0) {
+            player.TakeDamage(trapDamage);
+            Debug.Log(player.playerHealth);
+            frameTimer = 1;
+        }
+    }
     // Trap Damage on Hit
     void OnTriggerEnter2D(Collider2D col) {
         if (col.gameObject == player.gameObject) {
-            player.TakeDamage(trapDamage);
+            inTrap = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col) {
+        if (col.gameObject == player.gameObject) {
+            inTrap = false;
         }
     }
 }
